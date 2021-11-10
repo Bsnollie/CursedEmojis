feaa84bfa4b073683d23b2a68bf57f8c858b1dc0
Rodrigo Soriano <rodrigo_skate9@hotmail.com>
2021-11-10 00:10:16 -0600

    Add project files.

---
+++ CursedEmojis/CursedEmojis.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.IO;
+
+namespace DuckGame.CursedEmojis
+{
+    public class CursedEmojis : ClientMod
+    {
+        public override Priority priority
+        {
+            get
+            {
+                return Priority.HatPack;
+            }
+        }
+
+        protected override void OnPostInitialize()
+        {
+            foreach (string file in Directory.GetFiles(configuration.contentDirectory + "/hats"))
+            {
+                Team team = Team.Deserialize(file);
+                if (team != null) Teams.AddExtraTeam(team);
+            }
+
+            base.OnPostInitialize();
+        }
+    }
+}
---
+++ CursedEmojis/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+// The title of your mod, as displayed in menus
+[assembly: AssemblyTitle("|DGORANGE|Cursed Emojis")]
+
+// The author of the mod
+[assembly: AssemblyCompany("Bsnollie")]
+
+// The description of the mod
+[assembly: AssemblyDescription("|DGORANGE|15|WHITE|+ Hats included!")]
+
+// The mod's version
+[assembly: AssemblyVersion("1.0.0.0")]
